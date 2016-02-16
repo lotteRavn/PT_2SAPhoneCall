458d7fc0532d638b0438ac2f532eff4a63e9bc40
lotteRavn <lotteravn@Lottes-Mac-mini.local>
2016-02-16 10:27:04 +0100

    KontaktCall

--- PhoneCall/ViewController.swift
+++ PhoneCall/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
 
+    @IBOutlet weak var txtPhoneNumber: UITextField!
+    
+    
+    @IBOutlet weak var txtSMS: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func callPhone(sender: AnyObject) {
+        
+        if let phoneUrl = NSURL(string: "tel://\(31531926)")
+        {
+            UIApplication.sharedApplication().openURL(phoneUrl)
+        }
 
+        
+    }
+
+    @IBAction func sendSMS(sender: AnyObject) {
+        
+        let smsVC = MFMessageComposeViewController()
+        
+        smsVC.body = txtSMS.text!
+        smsVC.recipients = [txtPhoneNumber.text!]
+        smsVC.messageComposeDelegate = self
+        
+        self.presentViewController(smsVC, animated: true, completion: nil)
+        
+    }
+    
+    func messageComposeViewController(controller: MFMessageComposeViewController, didFinishWithResult result: MessageComposeResult) {
+        
+    }
 
 }
 
